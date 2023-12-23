a7d713dbddd1939562846688fc797a91fe5d2cdd
jagadeeswar-N-G <jagadeeswar8055@gmail.com>
2023-12-23 14:41:40 +0530

    removed the seller section

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,54 +1,38 @@
-'use client'
+"use client";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowRight, Loader2 } from "lucide-react";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
 
-import {
-  Button,
-  buttonVariants,
-} from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { cn } from '@/lib/utils'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { ArrowRight, Loader2 } from 'lucide-react'
-import Link from 'next/link'
-import { useForm } from 'react-hook-form'
+import { trpc } from "@/trpc/client";
 
-
-import { trpc } from '@/trpc/client'
-
-import { ZodError } from 'zod'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { Icons } from '@/components/ui/Icons'
-import { z } from "zod"
-import { useToast } from "@/components/ui/use-toast"
-import { useAuth } from '@/components/authProvider'
+import { ZodError } from "zod";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Icons } from "@/components/ui/Icons";
+import { z } from "zod";
+import { useToast } from "@/components/ui/use-toast";
+import { useAuth } from "@/components/authProvider";
 
 const Page = () => {
   const { login } = useAuth();
-  const { toast } = useToast()
-  const searchParams = useSearchParams()
-  const router = useRouter()
-  const isSeller = searchParams.get('as') === 'seller'
-  const origin = searchParams.get('origin')
+  const { toast } = useToast();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const isSeller = searchParams.get("as") === "seller";
+  const origin = searchParams.get("origin");
 
-  const continueAsSeller = () => {
-    router.push('?as=seller')
-  }
+  const AuthCredentialsValidator = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters long.",
+    }),
+  });
 
-  const continueAsBuyer = () => {
-    router.replace('/sign-in', undefined)
-  }
-
-
-const AuthCredentialsValidator = z.object({
-  email: z.string().email(),
-  password: z.string().min(8, {
-    message: 'Password must be at least 8 characters long.',
-  }),
-})
-
-type TAuthCredentialsValidator = z.infer<
-  typeof AuthCredentialsValidator
->
+  type TAuthCredentialsValidator = z.infer<typeof AuthCredentialsValidator>;
 
   const {
     register,
@@ -56,119 +40,82 @@ type TAuthCredentialsValidator = z.infer<
     formState: { errors },
   } = useForm<TAuthCredentialsValidator>({
     resolver: zodResolver(AuthCredentialsValidator),
-  })
+  });
 
-
-  const onSubmit = ({
-    email,
-    password,
-  }: TAuthCredentialsValidator) => {
-    login()
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
+    login();
     toast({
-      description:"You have signed in succesfully",
-    })
-    router.push('/')
-  }
+      description: "You have signed in succesfully",
+    });
+    router.push("/");
+  };
 
   return (
     <>
-      <div className='container relative flex pt-20 flex-col items-center justify-center lg:px-0'>
-        <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
-          <div className='flex flex-col items-center space-y-2 text-center'>
-            <Icons.logo className='h-20 w-20' />
-            <h1 className='text-2xl font-semibold tracking-tight'>
-              Sign in to your {isSeller ? 'seller' : ''}{' '}
-              account
+      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col items-center space-y-2 text-center">
+            <Icons.logo className="h-20 w-20" />
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Sign in to your {isSeller ? "seller" : ""} account
             </h1>
 
             <Link
               className={buttonVariants({
-                variant: 'link',
-                className: 'gap-1.5',
+                variant: "link",
+                className: "gap-1.5",
               })}
-              href='/sign-up'>
+              href="/sign-up"
+            >
               Don&apos;t have an account?
-              <ArrowRight className='h-4 w-4' />
+              <ArrowRight className="h-4 w-4" />
             </Link>
           </div>
 
-          <div className='grid gap-6'>
+          <div className="grid gap-6">
             <form onSubmit={handleSubmit(onSubmit)}>
-              <div className='grid gap-2'>
-                <div className='grid gap-1 py-2'>
-                  <Label htmlFor='email'>Email</Label>
+              <div className="grid gap-2">
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="email">Email</Label>
                   <Input
-                    {...register('email')}
+                    {...register("email")}
                     className={cn({
-                      'focus-visible:ring-red-500':
-                        errors.email,
+                      "focus-visible:ring-red-500": errors.email,
                     })}
-                    placeholder='you@example.com'
+                    placeholder="you@example.com"
                   />
                   {errors?.email && (
-                    <p className='text-sm text-red-500'>
+                    <p className="text-sm text-red-500">
                       {errors.email.message}
                     </p>
                   )}
                 </div>
 
-                <div className='grid gap-1 py-2'>
-                  <Label htmlFor='password'>Password</Label>
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="password">Password</Label>
                   <Input
-                    {...register('password')}
-                    type='password'
+                    {...register("password")}
+                    type="password"
                     className={cn({
-                      'focus-visible:ring-red-500':
-                        errors.password,
+                      "focus-visible:ring-red-500": errors.password,
                     })}
-                    placeholder='Password'
+                    placeholder="Password"
                   />
                   {errors?.password && (
-                    <p className='text-sm text-red-500'>
+                    <p className="text-sm text-red-500">
                       {errors.password.message}
                     </p>
                   )}
                 </div>
 
-                <Button >
-                  Sign in
-                </Button>
+                <Button>Sign in</Button>
               </div>
             </form>
-
-            <div className='relative'>
-              <div
-                aria-hidden='true'
-                className='absolute inset-0 flex items-center'>
-                <span className='w-full border-t' />
-              </div>
-              <div className='relative flex justify-center text-xs uppercase'>
-                <span className='bg-background px-2 text-muted-foreground'>
-                  or
-                </span>
-              </div>
-            </div>
-
-            {isSeller ? (
-              <Button
-                onClick={continueAsBuyer}
-                variant='secondary'
-                disabled={false}>
-                Continue as customer
-              </Button>
-            ) : (
-              <Button
-                onClick={continueAsSeller}
-                variant='secondary'
-                disabled={false}>
-                Continue as seller
-              </Button>
-            )}
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
